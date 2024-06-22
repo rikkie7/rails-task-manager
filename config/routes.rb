d1481a7195fe2d389d51b02549dfaa90a9880cf0
Ruiqi Guan <rickyguan1994@hotmail.com>
2024-06-22 15:57:34 +1000

    challenge complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
   get "tasks", to: "tasks#index"
   get "tasks/new", to: "tasks#new"
   get "tasks/:id", to: "tasks#show", as: :task
